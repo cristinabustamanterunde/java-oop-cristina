8560a6e18c852ff53147d27dfc9b97bf93447b64
“cristinabustamanterunde” <cristina.bustamanterunde@gmail.com>
2023-01-17 23:28:03 +0100

    errores

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,6 +3,7 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
+        Product producto1 = new Product("Anillo Sello Turquesa", 30.50d, "anillo", "plata", 8.00f,2,"Uno de 50");
 
 
     }
