d5c37e75b56d54f2ce4c9ecaa83173561d2189d7
“cristinabustamanterunde” <cristina.bustamanterunde@gmail.com>
2023-01-18 00:38:20 +0100

    Pruebas 5 (lleno de errores)

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,8 +3,12 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-        Product producto1 = new Product("Anillo Sello Turquesa", 30.50d, "anillo", "plata", 8.00f,2,"Uno de 50");
+        Product joya1 = new Product("Anillo Sello Turquesa", 30.50d, "anillo", "plata", 8.00f,"Drora");
+        Product joya2 = new Product("Gargantilla flecha", 18.00d,"collar","zamac",2f,"Ophira");
+        Product joya3 = new Product("Broche pez", 38.75d, "broche","cristales",0.00f,"Victorio&Luccino");
+        Product joya4 = new Product("Pendientes Granada", 25.60d, "pendientes","piedras",0.00f,"Drora");
+        Product joya5 = new Product("Broche Corazón", 15.75d,"broche","plata", 0.00f,"Ophira");
 
 
-    }
+      }
 }
